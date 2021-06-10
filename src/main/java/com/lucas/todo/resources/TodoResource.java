484d4379225d1e5c1d5ec9353d47554ec2c49948
Lucas França <lucas.franca.fagundes@gmail.com>
2021-06-09 23:34:43 -0300

    Adicionando classes Resources e Services

--- src/main/java/com/lucas/todo/resources/TodoResource.java
+++ src/main/java/com/lucas/todo/resources/TodoResource.java
@@ -1,5 +1,6 @@
 package com.lucas.todo.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,13 +8,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucas.todo.domain.Todo;
+import com.lucas.todo.services.TodoService;
 
 @RestController
 @RequestMapping(value = "/todos")
 public class TodoResource {
+	
+	@Autowired
+	private TodoService service;
+
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Todo> findById(PathVariable Integer id) {
-		
+		Todo obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
+
--- src/main/java/com/lucas/todo/services/TodoService.java
+++ src/main/java/com/lucas/todo/services/TodoService.java
@@ -2,14 +2,22 @@ package com.lucas.todo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.Optional;
+import com.lucas.todo.domain.Todo;
 import com.lucas.todo.repositories.TodoRepository;
+
+//import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
 
 @Service
 public class TodoService {
 
 	@Autowired
 	private TodoRepository repository;
+	
+	public Todo findById(Integer id) {
+		Optional<Todo> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
 	
 	
 }
