9980e024a7b43572c9ee48224248840fe2d78eb8
Lucas França <lucas.franca.fagundes@gmail.com>
2021-09-06 21:06:39 -0300

    Metodo delete

--- front/todo/src/app/components/read-all/read-all.component.ts
+++ front/todo/src/app/components/read-all/read-all.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { Todo } from "src/app/models/todo";
 import { TodoService } from "src/app/services/todo.service";
 
@@ -13,7 +14,7 @@ export class ReadAllComponent implements OnInit {
   
   list: Todo[] = [];
   listFinished: Todo[] = [];
-  constructor(private service: TodoService) {}
+  constructor(private service: TodoService,private router:Router) {}
 
   
   /**Methods */
@@ -44,4 +45,14 @@ export class ReadAllComponent implements OnInit {
       }
     }
   }*/
+
+delete(id: any): void{
+  this.service.delete(id).subscribe((resposta) =>{
+    if (resposta ===null){
+      this.service.message('Task deletada com sucesso!');
+      this.list = this.list.filter((todo) => todo.id !== id);
+    }
+  })
+}
+
 }
